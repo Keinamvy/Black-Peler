81518c5f75efcfe08bf8ebd4fb30cb5bdae5832d
Sultan Alsawaf <sultan@kerneltoast.com>
2023-11-13 23:11:35 -0800

    cpufreq: schedutil: Set default up/down rate limits to 500/1000 us
    This is empirically observed to yield good performance with reduced power
    consumption via having the down rate limit configured to be 2x longer than
    the up rate limit. This reduces bouncing between CPU frequencies by
    stalling down-clocking, which not only improves performance, but also
    counter-intuitively improves power consumption.
    
    The short up/down rate limits also provide improved interactivity and
    real-time response.
    
    Change-Id: I0ffa7bf56898fde166b4f9b327491bc73848be93
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -773,24 +773,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-
-	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us =
-					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
-		tunables->down_rate_limit_us =
-					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_BIG;
-	}
-
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us =
-					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LITTLE;
-		tunables->down_rate_limit_us =
-					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LITTLE;
-	}
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 1000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
