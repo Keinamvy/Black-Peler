0061a45fd9fff42518052328846bb6bcd21cb6f2
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-09-09 21:02:57 +0700

    treewide: import oplus changes
    squashed from oxygen by dandaa,
    removed a lot of shit and derps.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- net/socket.c
+++ net/socket.c
@@ -111,6 +111,10 @@
 #include <net/busy_poll.h>
 #include <linux/errqueue.h>
 
+#ifdef CONFIG_OPLUS_NWPOWER
+#include <net/oplus_nwpower.h>
+#endif
+
 #ifdef CONFIG_NET_RX_BUSY_POLL
 unsigned int sysctl_net_busy_read __read_mostly;
 unsigned int sysctl_net_busy_poll __read_mostly;
@@ -414,6 +418,10 @@ struct file *sock_alloc_file(struct socket *sock, int flags, const char *dname)
 	struct qstr name = { .name = "" };
 	struct path path;
 	struct file *file;
+#ifdef CONFIG_OPLUS_NWPOWER
+	struct pid *pid;
+	struct task_struct *task;
+#endif
 
 	if (dname) {
 		name.name = dname;
@@ -441,6 +449,22 @@ struct file *sock_alloc_file(struct socket *sock, int flags, const char *dname)
 	sock->file = file;
 	file->f_flags = O_RDWR | (flags & O_NONBLOCK);
 	file->private_data = sock;
+#ifdef CONFIG_OPLUS_NWPOWER
+	pid = find_get_pid(current->tgid);
+	if (pid) {
+		task = get_pid_task(pid, PIDTYPE_PID);
+		if (task && sock->sk) {
+			strncpy(sock->sk->sk_cmdline, task->comm, TASK_COMM_LEN);
+			sock->sk->sk_cmdline[TASK_COMM_LEN - 1] = 0;
+		}
+		put_task_struct(task);
+	}
+	put_pid(pid);
+
+	if (sock->sk) {
+		sock->sk->sk_oplus_pid = current->tgid;
+	}
+#endif
 	return file;
 }
 EXPORT_SYMBOL(sock_alloc_file);
@@ -1681,6 +1705,10 @@ SYSCALL_DEFINE3(connect, int, fd, struct sockaddr __user *, uservaddr,
 	if (err < 0)
 		goto out_put;
 
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_OUTPUT, sock))
+		return -EACCES;
+#endif
 	err =
 	    security_socket_connect(sock, (struct sockaddr *)&address, addrlen);
 	if (err)
@@ -1784,6 +1812,11 @@ SYSCALL_DEFINE6(sendto, int, fd, void __user *, buff, size_t, len,
 	if (!sock)
 		goto out;
 
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_OUTPUT, sock))
+		return -EACCES;
+#endif
+
 	msg.msg_name = NULL;
 	msg.msg_control = NULL;
 	msg.msg_controllen = 0;
@@ -1839,6 +1872,11 @@ SYSCALL_DEFINE6(recvfrom, int, fd, void __user *, ubuf, size_t, size,
 	sock = sockfd_lookup_light(fd, &err, &fput_needed);
 	if (!sock)
 		goto out;
+
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_INPUT, sock))
+		return err;
+#endif
 
 	msg.msg_control = NULL;
 	msg.msg_controllen = 0;
@@ -2176,6 +2214,11 @@ long __sys_sendmsg(int fd, struct user_msghdr __user *msg, unsigned flags)
 	if (!sock)
 		goto out;
 
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_OUTPUT, sock))
+		return -EACCES;
+#endif
+
 	err = ___sys_sendmsg(sock, msg, &msg_sys, flags, NULL, 0);
 
 	fput_light(sock->file, fput_needed);
@@ -2216,6 +2259,12 @@ int __sys_sendmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 
 	used_address.name_len = UINT_MAX;
 	entry = mmsg;
+
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_OUTPUT, sock))
+		return -EACCES;
+#endif
+
 	compat_entry = (struct compat_mmsghdr __user *)mmsg;
 	err = 0;
 	flags |= MSG_BATCH;
@@ -2346,6 +2395,11 @@ long __sys_recvmsg(int fd, struct user_msghdr __user *msg, unsigned flags)
 	if (!sock)
 		goto out;
 
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_INPUT, sock))
+		return err;
+#endif
+
 	err = ___sys_recvmsg(sock, msg, &msg_sys, flags, 0);
 
 	fput_light(sock->file, fput_needed);
@@ -2386,6 +2440,11 @@ int __sys_recvmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 	sock = sockfd_lookup_light(fd, &err, &fput_needed);
 	if (!sock)
 		return err;
+
+#ifdef CONFIG_OPLUS_NWPOWER
+	if(oplus_check_socket_in_blacklist(OPLUS_NET_INPUT, sock))
+		return err;
+#endif
 
 	err = sock_error(sock->sk);
 	if (err) {
